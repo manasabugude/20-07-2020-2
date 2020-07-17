25588ec5bd18d4d1af6d87b1ba5c5b4441b9a3cf
manasabugude <manasabugude1999@gmail.con>
2020-07-17 18:31:16 +0530

    practice

---
+++ 17-07-2020-5.java
@@ -0,0 +1,18 @@
+public class TryCatch 
+{  
+public static void main(String args[]) 
+{  
+int i=100;  
+int j=0;  
+int data;  
+try  
+{  
+data=i/j;    
+}   
+catch(Exception e)  
+{  
+         
+System.out.println(i/(j+2));  
+}  
+}  
+}  
