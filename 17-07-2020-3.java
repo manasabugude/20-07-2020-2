3c77f1d156ad89c1283c3034af6551c671712cab
manasabugude <manasabugude1999@gmail.con>
2020-07-17 13:29:19 +0530

    practice

---
+++ 17-07-2020-3.java
@@ -0,0 +1,23 @@
+public class MyClass
+{
+public static void main(String args[])
+{
+byte myByte=100;
+short myShort=500;
+long myLong=1000;
+int myNum=10;
+double myDouble=15.55d;
+float myFloat=2.66f;
+boolean myBool=true;
+System.out.println(myByte);
+System.out.println(myShort);
+System.out.println(myLong);
+System.out.println(myNum);
+System.out.println(myDouble);
+System.out.println(myFloat);
+System.out.println(myBool);
+}
+}
+
+
+
