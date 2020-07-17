f8dbc1b2a61730d6eab747be2382cec20d120fc3
manasabugude <manasabugude1999@gmail.com>
2020-07-17 08:59:23 +0530

    practice

---
+++ 17-07-2020abstraction.java
@@ -0,0 +1,18 @@
+abstract class Bank{    
+abstract int getRateOfInterest();    
+}    
+class ABI extends Bank{    
+int getRateOfInterest(){return 7;}    
+}    
+class PNB extends Bank{    
+int getRateOfInterest(){return 8;}    
+}    
+    
+class TestBank{    
+public static void main(String args[]){    
+Bank b;  
+b=new ABI();  
+System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+b=new PNB();  
+System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");    
+}}    
