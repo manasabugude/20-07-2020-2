18d47445bf41eabd8fd30373bc9cc0d834f061c1
manasabugude <manasabugude1999@gmail.com>
2020-07-18 14:48:09 +0530

    practice

---
+++ 18-07-2020-5.java
@@ -0,0 +1,27 @@
+import java.util.*;  
+public class Priority
+{  
+public static void main(String args[]){  
+PriorityQueue<String> queue=new PriorityQueue<String>();  
+queue.add("dog");  
+queue.add("cat");  
+queue.add("cow");  
+queue.add("rat");  
+System.out.println("head:"+queue.element());  
+System.out.println("head:"+queue.peek());  
+System.out.println("iterating the queue elements:");  
+Iterator itr=queue.iterator();  
+while(itr.hasNext())
+{  
+System.out.println(itr.next());  
+}  
+queue.remove();  
+queue.poll();  
+System.out.println("after removing two elements:");  
+Iterator<String> itr2=queue.iterator();  
+while(itr2.hasNext())
+{  
+System.out.println(itr2.next());  
+}  
+}  
+}  
