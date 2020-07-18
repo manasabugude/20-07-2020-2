bb736810318514c0e1b48eaaf325cce7eb66a523
manasabugude <manasabugude1999@gmail.com>
2020-07-18 16:07:58 +0530

    practice

---
+++ 18-07-2020-8.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashSet;
+public class LinkedHashSet1 
+{
+public static void main(String args[]) 
+{
+LinkedHashSet<String> lhset = new LinkedHashSet<String>();
+lhset.add("Z");
+lhset.add("PQ");
+lhset.add("N");
+lhset.add("O");
+lhset.add("KK");
+lhset.add("FGH");
+System.out.println(lhset);
+LinkedHashSet<Integer> lhset2 = new LinkedHashSet<Integer>();
+lhset2.add(99);
+lhset2.add(7);
+lhset2.add(0);
+lhset2.add(67);
+lhset2.add(89);
+lhset2.add(66);
+System.out.println(lhset2);
+}
+}
