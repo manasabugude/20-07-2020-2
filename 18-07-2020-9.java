2dbc2067172e7ae015f83979709a809901721f86
manasabugude <manasabugude1999@gmail.com>
2020-07-18 16:16:34 +0530

    practice

---
+++ 18-07-2020-9.java
@@ -0,0 +1,23 @@
+import java.util.TreeSet;
+public class TreeSet1
+{
+public static void main(String args[]) 
+{
+TreeSet<String> tset = new TreeSet<String>();
+tset.add("ABC");
+tset.add("String");
+tset.add("Test");
+tset.add("Pen");
+tset.add("Ink");
+tset.add("Jack");
+System.out.println(tset);
+TreeSet<Integer> tset2 = new TreeSet<Integer>();
+tset2.add(88);
+tset2.add(7);
+tset2.add(101);
+tset2.add(0);
+tset2.add(3);
+tset2.add(222);
+System.out.println(tset2);
+}
+}
