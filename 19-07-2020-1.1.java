91de255be3c7bb6949a94d23dd13baf31e6f17b7
manasabugude <manasabugude1999@gmail.com>
2020-07-19 12:13:19 +0530

    practice

---
+++ 19-07-2020-1.1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class Main
+{
+public static void main(String args[])
+{
+ArrayList<Integer>list1=new ArrayList<>();
+list1.add(3);
+list1.add(5);
+System.out.println("arraylist of integer :" +list1);
+
+ArrayList<String>list2=new ArrayList<>();
+list2.add("Apple");
+list2.add("banana");
+System.out.println("arraylist of string :" +list2);
+
+ArrayList<Double>list3=new ArrayList<>();
+list3.add(5.6);
+list3.add(2.3);
+System.out.println("arraylist of double :" +list3);
+}
+}
