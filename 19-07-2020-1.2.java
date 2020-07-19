8ae0600ce57c766fa8cda7b91182692d139227cd
manasabugude <manasabugude1999@gmail.com>
2020-07-19 12:27:08 +0530

    practice

---
+++ 19-07-2020-1.2.java
@@ -0,0 +1,20 @@
+public class Main1 
+{
+public static void main(String[] args) 
+{
+GenericsClass<Integer> intObj = new GenericsClass<>(5);
+System.out.println("Generic Class returns: " + intObj.getData());
+GenericsClass<String> stringObj = new GenericsClass<>("Java Programming");
+System.out.println("Generic Class returns: " + stringObj.getData());
+}
+}
+
+class GenericsClass<T> {
+private T data;
+public GenericsClass(T data) {
+this.data = data;
+}
+public T getData() {
+return this.data;
+}
+}
