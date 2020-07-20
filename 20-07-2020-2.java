213ef4128825c3c827f1ad6bba3cea38fb8dd6c6
manasabugude <manasabugude1999@gmail.com>
2020-07-20 12:25:13 +0530

    practice

---
+++ 20-07-2020-2.java
@@ -0,0 +1,23 @@
+package com.java.serialization;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+public class DeSerializingObject {
+public static void main(String[] args) {
+Employee employeeInput = null;
+FileInputStream fis = null;
+ObjectInputStream ois = null;
+try {
+fis = new FileInputStream("Employee.ser");
+ois = new ObjectInputStream(fis);
+employeeInput = (Employee)ois.readObject();
+System.out.println("Serialized data is restored from Employee.ser file");
+ois.close();
+fis.close();
+} catch (IOException | ClassNotFoundException e) {
+e.printStackTrace();
+} 
+System.out.println("Name of employee is : " + employeeInput.getSerializeValueName());
+System.out.println("Salary of employee is : " + employeeInput.getNonSerializeValueSalary());
+}
+}
